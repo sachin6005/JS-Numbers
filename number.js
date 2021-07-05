9a2d8245f9e4e7607df0f0a1f631c870f94a7760
sachin kumar <merugune.sachin123@gmail.com>
2021-07-06 00:03:24 +0530

    javascript numbers calculation

---
+++ number.js
@@ -0,0 +1,45 @@
+console.clear()
+
+var num1 = 0
+var num2 = 0
+
+console.log(num1/num2)
+
+
+var num1 = 0
+var num2 = 5
+
+console.log(num1/num2)
+
+
+var num1 = 0
+var name = 'a'
+
+console.log(num1*name)
+
+
+
+var num1 = 'a'
+var num2 = 3
+
+console.log(num1+num2)
+
+
+
+var num1 = 'b'
+var num2 = 4
+
+console.log(num1+num2)
+
+
+
+
+console.log(4+'b')
+
+
+
+console.log(5-'c')
+
+
+
+console.log('c'-5)
